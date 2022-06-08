06f22973235cbf8778773c60201ef4b7a633283a
LenaBGitHub <hbogdanovich@gmail.com>
2022-06-08 22:43:14 +0300

    modified:   HT_13.py

--- HT_13.py
+++ HT_13.py
@@ -1,112 +1,102 @@
 import random
 
 
-# HT13_1
-# Из полученного списка чисел создайте список с суммами
-# этих чисел, отсортированными по возрастанию
+def list_sum_nums(list_nums):
+    """Из полученного списка чисел создайте список с суммами
+этих чисел, отсортированными по возрастанию"""
+    ls_sum_nums = []
+    for i in list_nums:
+        sum_i = 0
+        while i != 0:
+            sum_i += i % 10
+            i = i // 10
+        ls_sum_nums.append(sum_i)
+
+    return sorted(ls_sum_nums)
 
 
-# def list_sum_nums(list_nums):
-#     ls_sum_nums = []
-#     for i in list_nums:
-#         sum_i = 0
-#         while i != 0:
-#             sum_i += i % 10
-#             i = i // 10
-#         ls_sum_nums.append(sum_i)
-#
-#     return sorted(ls_sum_nums)
-#
-#
-# print(list_sum_nums([int(i) for i in input().split()]))
-
-# HT13_2
-# Напишите функцию f(x), которая возвращает значение следующей функции, определённой на всей числовой прямой
+print(list_sum_nums([int(i) for i in input().split()]))
 
 
-# def f(x):
-#     if x <= -2:
-#         return 1 - (x + 2)**2
-#     elif -2 < x <= 2:
-#         return -(x / 2)
-#     elif 2 < x:
-#         return (x - 2)**2 + 1
-#
-#
-# print(f(float(input())))
-
-# HT_13_3
-# Напишите функцию, которая принимает на вход список целых чисел, удаляет из него
-# все нечётные значения, а чётные нацело делит на два.
+def f(x):
+    """Функция f(x), которая возвращает значение следующей функции, определённой на всей числовой прямой"""
+    if x <= -2:
+        return 1 - (x + 2)**2
+    elif -2 < x <= 2:
+        return -(x / 2)
+    elif 2 < x:
+        return (x - 2)**2 + 1
 
 
-# def even_nums(list_nums):
-#     return [i // 2 for i in list_nums if i % 2 == 0]
-#
-#
-# print(even_nums([int(i) for i in input().split()]))
+print(f(float(input())))
 
-# HT_13_4
-# Напишите функцию, которая создает список случайных элементов. На вход функция принимает кол-во элементов,
-# минимальное и максимальное значение
-# def rand_nums(count, min_num, max_nam):
-#     ls_nums = [random.randint(min_num, max_nam) for i in range(count)]
-#     return ls_nums
-#
-#
-# a, b, c = map(int, input().split())
-# print(rand_nums(a, b, c))
 
-# HT_13_5
-# Преобразуйте задачу с покупкой торта из экзамена 2 в функцию.
-# cakes = {'наполеон': ['тесто сахар масло', 5, 1000],
-#          'медовик': ['тесто мед сгущенка', 4, 950],
-#          'киевский': ['тесто орехи безе', 7, 800]}
-#
-#
-# def buy_cake(cake, request, count):
-#     if request == 'описание':
-#         print(f'Торт {cake} состоит из {cakes[cake][0]}')
-#     elif request == 'цена':
-#         print(f'Торт {cake} стоит {cakes[cake][1]} рублей')
-#     elif request == 'количество':
-#         print(f'Торт {cake} осталось {cakes[cake][2]} грамм')
-#     print(f'Торта {cake} осталось {cakes[cake][2] - count} грамм')
-#     return f'К оплате {cakes[cake][1] * count / 100} рублей'
-#
-#
-# ck = input('Какой торт Вы хотели бы приобрести: ').lower()
-# rst = input('Что Вы хотели бы уточнить: ').lower()
-# cnt = int(input('Сколько торта Вам положить, гр.: '))
-# print(buy_cake(ck, rst, cnt))
+def even_nums(list_nums):
+    """Принимает на вход список целых чисел, удаляет из него
+    все нечётные значения, а чётные нацело делит на два"""
+    return [i // 2 for i in list_nums if i % 2 == 0]
 
-# HT_13_6
-# Напишите функцию, вычисляющую значение факториала числа N. Используйте рекурсию
-# def factorial(n):
-#     if n == 0:
-#         return 1
-#     return factorial(n - 1) * n
-#
-#
-# print(factorial(int(input())))
 
-# 13_11
+print(even_nums([int(i) for i in input().split()]))
+
+
+def rand_nums(count, min_num, max_nam):
+    """Функция, которая создает список случайных элементов. На вход функция принимает кол-во элементов,
+минимальное и максимальное значение"""
+    return [random.randint(min_num, max_nam) for i in range(count)]
+
+
+a, b, c = map(int, input().split())
+print(rand_nums(a, b, c))
+
+cakes = {'наполеон': ['тесто сахар масло', 5, 1000],
+         'медовик': ['тесто мед сгущенка', 4, 950],
+         'киевский': ['тесто орехи безе', 7, 800]}
+
+
+def buy_cake(cake, request, count):
+
+    """Функция покупки торта:
+Принимает название торта, запрос, и количество,
+выводит стоимость"""
+
+    if request == 'описание':
+        print(f'Торт {cake} состоит из {cakes[cake][0]}')
+    elif request == 'цена':
+        print(f'Торт {cake} стоит {cakes[cake][1]} рублей')
+    elif request == 'количество':
+        print(f'Торт {cake} осталось {cakes[cake][2]} грамм')
+    print(f'Торта {cake} осталось {cakes[cake][2] - count} грамм')
+    return f'К оплате {cakes[cake][1] * count / 100} рублей'
+
+
+ck = input('Какой торт Вы хотели бы приобрести: ').lower()
+rst = input('Что Вы хотели бы уточнить: ').lower()
+cnt = int(input('Сколько торта Вам положить, гр.: '))
+print(buy_cake(ck, rst, cnt))
+
+
+def factorial(n):
+    """Функция, вычисляющая значение факториала числа N"""
+    if n == 0:
+        return 1
+    return factorial(n - 1) * n
+
+
+print(factorial(int(input())))
+
+
 def decor(f_to_be_decor):
     def wrapper_func(param1, param2):
-        n1 = param2
-        n2 = param1
-        return n1 / n2
+        return param2 / param1
     return wrapper_func
 
 
 @decor
-def div(a, b):
-    return a/b
+def div(first, second):
+    return first / second
 
 
-first = int(input())
-second = int(input())
-print(div(first, second))
-
-
-
+n1 = int(input())
+n2 = int(input())
+print(div(n1, n2))
